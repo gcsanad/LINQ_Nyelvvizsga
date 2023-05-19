9a97074bb9b7687e50969f519628cf8125749cbb
gcsanad <71721197+gcsanad@users.noreply.github.com>
2023-05-19 09:03:42 +0200

    Add files via upload

---
+++ ConsoleApp1/Nyelvvizsga.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Nyelvvizsga
+    {
+        public List<int> sikeres;
+        public List<int> sikertelen;
+        public string nyelv;
+        public List<int> osszesitve;
+        public Nyelvvizsga(string elsoSor, string masodikSor)
+        {
+            string[] elso = elsoSor.Split(';');
+            string[] masodik = masodikSor.Split(';');
+            nyelv = elso[0];
+            sikeres = new List<int>();
+            sikertelen = new List<int>();
+            osszesitve = new List<int>();
+            for (int index = 0; index < elso.Length; index++)
+            {
+                sikeres.Add(Convert.ToInt32(elso[index]));
+                sikertelen.Add(Convert.ToInt32(masodik[index]));
+                osszesitve.Add(sikeres[index - 1] + sikertelen[index - 1]);
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿//1.Feladat
+using ConsoleApp1;
+using System.Text;
+using System.Text.Unicode;
+
+string[] sikeresek = File.ReadAllLines("sikeres.csv", Encoding.UTF8);
+string[] sikertelen = File.ReadAllLines("sikertelen.csv", Encoding.UTF8);
+List<Nyelvvizsga> osszesitve = new List<Nyelvvizsga>();
+for (int index = 0; index < sikeresek.Length; index++)
+{
+    osszesitve.Add(new Nyelvvizsga(sikeresek[index], sikertelen[index]));
+}
+
+//2.Feladat
+var legnepszerubbNyelvek = osszesitve.OrderByDescending(x => x.osszesitve).Take(3).ToList();
+Console.WriteLine("2. feladat: A legnépszerűbb nyelvek:");
+foreach (var elem in legnepszerubbNyelvek)
+{
+    Console.WriteLine(elem.nyelv);
+}
+
+//3.Feladat
+Console.WriteLine("3. feladat");
+Console.Write("Adjon meg egy évszámot!");
+int evszam = Convert.ToInt32(Console.ReadLine());
+if (evszam !> 2009 || evszam !< 2017)
+{
+    do
+    {
+        Console.Write("Hibás évszám, kérem adjon másikat!");
+        evszam = Convert.ToInt32(Console.ReadLine());
+    } while (evszam !> 2009 || evszam !< 2017);
+}
+
+//4.Feladat
